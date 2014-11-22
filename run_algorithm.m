2049a89d530cf3a1a97460367f82145523117a86
rocket-raccoon <the.alberto.bena@gmail.com>
2014-11-21 19:29:32 -0500

    Added the LASSO step

--- run_algorithm.m
+++ run_algorithm.m
@@ -1,33 +1,35 @@
 function [ omega, theta ] = run_algorithm(file_path, lambda)
     %Get the Y matrix and associated dimensions
-    Y = csvread(file_path,1,1);
+    %Y = csvread(file_path,1,1);
+    Y = csvread(file_path, 1,1, [1 1 10 20]);
     [n, p] = size(Y);
     %Initialize guesses for parameters
     omega = eye(p);
     omega_new = omega;
-    theta = rand(p,2);
+    theta = .5.*ones(p,2);
     theta_new = theta;
     %Iterate until convergence
     while(1)
         %Create Big L
         bigL = create_bigL(theta,n);
         size(bigL)
-        size(omega)
-        size(Y)
         %Create Big M
         littleM = create_littleM(n);
         bigM = create_bigM(littleM, p);
+        size(bigM)
         %Create Psi
-        X = bigM\bigL;
+        X = inv(bigL)*bigM;
         size(X)
-        Yhat = (vec(Y*omega)\bigL)';
+        Yhat = (inv(bigL)*vec(Y*omega));
         size(Yhat)
-        size(X' \ (X'*X))
-        psi = X'\(X'*X)*Yhat;
+        psi = inv(X'*X)*X'*Yhat;
         size(psi)
         %Update omega
-        omega_new = LassoBlockCoordinate(Y\bigL, -bigM\bigL*psi, lambda);
+        A = inv(bigL)*kron(eye(p),Y);
+        b = inv(bigL)*bigM*psi;
+        omega_new = LassoBlockCoordinate(A, b, lambda);
         omega_new
+        break;
         %Update theta
         theta = rand(p,2);
         %Get new objective function value
