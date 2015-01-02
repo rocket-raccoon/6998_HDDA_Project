232364f8b6bd907cc7875d9c6934745a8482fb1f
codekitty <tadmor.michelle@gmail.com>
2015-01-03 02:40:06 +0300

    Added new stock loader by sector, naive prediction method, and benchmark naive vs exponential

--- load_stock_data.m
+++ load_stock_data.m
@@ -1,38 +1,68 @@
-function [Y names]=load_stock_data()
+function [Y, stocknames, grouping, fmap]=load_stock_data()
+% Y - data
+% names - file name (assuming that is the stock ticker name)
+% grouping - an array grouping stocks by folder
     
+d=500;          % data entries to compile
+Y=zeros(d, 0);  % output variable for numerical data 
+stocknames = {};     % output variable for ticker name per stock
+grouping = [];  % output variable for sector grouping per stock
+fmap = containers.Map; % maps sector grouping to sector name
+
+% get user selected files
 files = uipickfiles('num',[1 inf],'out','cell', 'FilterSpec', ['*.csv']);
 if isequal(files,0) ~=0
     return 
+else 
+    new_files    = {};
+    remove_dirs = [];
+    % if any of the files are folder - replace them with inner contents
+    for f=1:numel(files)
+        if isdir(files{f})
+            tmp = struct2cell(dir([files{f} filesep '*.csv']));
+            new_filenames = tmp(1,:);
+            nfiles = numel(new_filenames);
+            new_files(end+1:end+nfiles) = strcat({[files{f} filesep]},new_filenames);
+            remove_dirs(end+1) = f;
+        end
+    end
+    files(remove_dirs) = [];
+    files(end+1:end+numel(new_files)) = new_files;   
 end
-
-tic;
-fids = cellfun(@(f) fopen(f, 'r'), files, 'UniformOutput', false);  %opening files
-csvheaders = cellfun(@(f) fgetl(f), fids, 'UniformOutput', false);   %readinf first line in file
-cellfun(@(f) fclose(f), fids, 'UniformOutput', false);  %closing files
-
-% %Convert  header to cell array
-% csvheaders = cellfun(@(h) regexp(h, '([^,]*)', 'tokens'), csvheaders, 'UniformOutput', false);
-% csvheaders = cellfun(@(h) cat(2, h{:}), csvheaders, 'UniformOutput', false);
-
 
 %Read in data    
+tic;
 csvdats = cellfun(@(fname) csvread(fname, 1,1), files, 'UniformOutput', false);
-
-%Add data to session
 disp(sprintf('Files loaded: %gs',toc));
 
+% group stocks by originating folder name - prepare folder name hash map   
+[foldername,~, ~] = fileparts(files{1});
+[~,sector, ~] = fileparts(foldername);
+fmap(sector) = 1;
 
-d=500;
-Y=zeros(d, 0);
-names = {};
+% foreach file copy the "d" length data, name, and grouping number
 for ci=1:numel(csvdats)
+    
+    % read data
     Y(:, end+1) = csvdats{ci}(1:d, 1);
-    [~, filename, ~] = fileparts(files{ci}); 
-    names{end+1} = filename;
+
+    [foldername, filename, ~] = fileparts(files{ci}); 
+    [~, sector, ~] = fileparts(foldername); 
+
+    % name of stock ticker
+    stocknames{end+1} = filename;
+    
+    % place a grouping per folder name
+    if ~isKey(fmap,sector)
+        g = max(grouping)+1;
+        fmap(sector) = g;
+    else
+        g = fmap(sector);
+    end
+    grouping(end+1) = g;
 end
 
+% reverse order 
 Y = Y(end:-1:1, :); % make time ascending
 
-% Y=cell2mat(csvdats);
-% Y = Y(:, 1:5:end);
 end
